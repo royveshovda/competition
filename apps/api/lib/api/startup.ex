dd0cf9bf3cdc0ed4e600b6c5e387d74860606581
Roy Veshovda <roy@veshovda.no>
2017-06-10 04:24:19 +0200

    Announce IP

--- apps/api/lib/api/startup.ex
+++ apps/api/lib/api/startup.ex
@@ -12,6 +12,8 @@ defmodule Api.Startup do
   end
 
   def handle_info(:ann, state) do
+    ips = get_ips()
+    Api.Slack.say_to_slack("Running on IP: " <> Enum.join(ips, " -- "))
     Api.Slack.say_to_slack("Starting up...I will serve the following questions:")
     Api.Slack.say_to_slack("Q1:  " <> Api.Questions.q1())
     Api.Slack.say_to_slack("A1:  " <> to_string(Api.Questions.a1()))
@@ -22,4 +24,23 @@ defmodule Api.Startup do
     {:stop, :normal, state}
   end
 
+  defp get_ips() do
+    {:ok, ips} = :inet.getif
+    IO.inspect ips
+    filter_ip_address(ips, [])
+  end
+
+  defp filter_ip_address([], acc) do
+    acc
+  end
+
+  defp filter_ip_address([{{127,0,0,1}, _broadcast, _mask} | tail], acc) do
+    filter_ip_address(tail, acc)
+  end
+
+  defp filter_ip_address([head | tail], acc) do
+    {{oct1, oct2, oct3, oct4}, _broadcast, _mask} = head
+    ip = to_string(oct1) <> "." <> to_string(oct2) <> "." <> to_string(oct3) <> "." <> to_string(oct4)
+    filter_ip_address(tail, acc ++ [ip])
+  end
 end
